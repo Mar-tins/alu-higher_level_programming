9c09524e44e078f82eea65a4b55d9f76343fa0b8
Mar-tins <b.martins@alustudent.com>
2025-02-24 19:29:18 -0800

    Fixed output and PEP8 issues for 6-concat.py

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str1 = "Welcome to"
-str2 = "Battery Street!"
-print(str1 + " " + str2)
+str2 = "Battery Street"
+str1 = str1 + " " + str2  # Concatenation
+print(str1 + "!")  # Printing the final message
