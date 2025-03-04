26cef9b6bc61b35dce133e6c95daa709cadd675e
Mar-tins <b.martins@alustudent.com>
2025-03-04 07:03:34 -0800

    Fix PEP8 whitespace issue in 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,7 +4,6 @@
 
 class Square:
     """Class that defines a square."""
-    
     def __init__(self, size):
         """Initialize a square with a private size attribute."""
         self.__size = size
