caddcd337e3fa5b34d3521ac11f715c6d43a75cf
Mar-tins <b.martins@alustudent.com>
2025-03-04 03:20:53 -0800

    Fixed safe_print_list with try/except and PEP8

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,10 +3,14 @@
 def safe_print_list(my_list=[], x=0):
     count = 0  # Counter for printed elements
     try:
-        for i in range(x):  # Loop x times
-            print(my_list[i], end="")  # Print without newline
-            count += 1  # Increase count
-    except IndexError:  # If we go out of bounds, do nothing
-        pass
-    print()  # Print newline after printing elements
-    return count  # Return number of elements printed
+        for i in range(x):
+            try:
+                print(my_list[i], end="")  # Print without newline
+                count += 1
+            except IndexError:
+                break  # Stop if index is out of range
+    except Exception:
+        pass  # Catch any unexpected errors
+
+    print()  # Print a newline after printing elements
+    return count  # Return the number of elements printed
