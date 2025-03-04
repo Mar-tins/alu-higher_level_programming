600bce03e66edd7a53212b752e8d707121607ac4
Mar-tins <b.martins@alustudent.com>
2025-03-04 03:56:22 -0800

    Fixed PEP8 line length issue

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """
+    Prints an integer using '{:d}'.format() and returns True if successful.
+    """
+    try:
+        print("{:d}".format(value))  # Format and print as an integer
+        return True
+    except (ValueError, TypeError):
+        return False
