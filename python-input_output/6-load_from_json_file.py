601f13e1ab87c1259e802cc802fd332f4a808218
Mar-tins <b.martins@alustudent.com>
2025-03-08 08:06:30 -0800

    Fix PEP8 issues, add documentation, and improve test cases in 6-load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,4 +1,11 @@
+#!/usr/bin/python3
 import json
+
+
+"""
+Module that contains a function to load an object from a JSON file.
+"""
+
 
 def load_from_json_file(filename):
     """
@@ -9,6 +16,10 @@ def load_from_json_file(filename):
 
     Returns:
         object: The object created from the JSON string.
+
+    Raises:
+        ValueError: If the JSON is not formatted correctly.
+        FileNotFoundError: If the file doesn't exist.
     """
     with open(filename, 'r') as file:
         return json.load(file)
